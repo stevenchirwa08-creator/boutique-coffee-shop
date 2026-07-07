4b9740cb9300021331930d690924ed7ac1897fca
steven <stevenchirwa08@gmail.com>
2026-07-07 19:45:49 +0200

    follow 13

--- javason.js
+++ javason.js
@@ -1,7 +1,34 @@
 
 function showDetails() {
     let details = document.getElementById("details");
-    if (details){
-    details.style.display ="block";
+    if (details) {
+        details.style.display = "block";
+    }
+
+    let details2 = document.getElementById("details2");
+    if (details2) {
+        details2.style.display = "block";
+    }
+
+     let details3 = document.getElementById("details3");
+    if (details3) {
+        details3.style.display = "block";
+    }
+
+     let details4 = document.getElementById("details4");
+    if (details4) {
+        details4.style.display = "block";
+    }
+
+     let details5 = document.getElementById("details5");
+    if (details5) {
+        details5.style.display = "block";
+    }
+
+     let details6 = document.getElementById("details6");
+    if (details6) {
+        details6.style.display = "block";
     }
 }
+
+

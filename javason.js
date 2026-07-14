9b41cccc737cbd65aee0a080b9f240ca671bf087
steven <stevenchirwa08@gmail.com>
2026-07-14 16:28:22 +0200

    follow 21

--- javason.js
+++ javason.js
@@ -1,35 +1,19 @@
 
-function showDetails() {
-    let details = document.getElementById("details");
-    if (details) {
-        details.style.display = "block";
+function toggleDetails(id) {
+    const element = document.getElementById(id);
+
+    if (element){
+        const currentDisplay = window.getComputedStyle(element).display;
+
+        if (currentDisplay === 'none'){
+            element.style.display = 'block';
+        } else {
+            element.style.display = 'none';
+        }
     }
 
-    let details2 = document.getElementById("details2");
-    if (details2) {
-        details2.style.display = "block";
-    }
-
-     let details3 = document.getElementById("details3");
-    if (details3) {
-        details3.style.display = "block";
-    }
-
-     let details4 = document.getElementById("details4");
-    if (details4) {
-        details4.style.display = "block";
-    }
-
-     let details5 = document.getElementById("details5");
-    if (details5) {
-        details5.style.display = "block";
-    }
-
-     let details6 = document.getElementById("details6");
-    if (details6) {
-        details6.style.display = "block";
-    }
 }
+
 function closeModal(){
     document.getElementById('discountModal').style.display ='none';
 closeModal();

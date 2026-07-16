e351d8c08d495a8305ee75a40b92e0cf89dd7740
steven <stevenchirwa08@gmail.com>
2026-07-16 11:37:45 +0200

    follow 24

--- javason.js
+++ javason.js
@@ -2,10 +2,10 @@
 function toggleDetails(id) {
     const element = document.getElementById(id);
 
-    if (element){
+    if (element) {
         const currentDisplay = window.getComputedStyle(element).display;
 
-        if (currentDisplay === 'none'){
+        if (currentDisplay === 'none') {
             element.style.display = 'block';
         } else {
             element.style.display = 'none';
@@ -14,26 +14,37 @@ function toggleDetails(id) {
 
 }
 
-function closeModal(){
-    document.getElementById('discountModal').style.display ='none';
-closeModal();
+function closeModal() {
+    document.getElementById('discountModal').style.display = 'none';
+    closeModal();
 }
-function closeSubscriptionModal(){
-    document.getElementById('subscription-modal').style.display ='none';
+function closeSubscriptionModal() {
+    document.getElementById('subscription-modal').style.display = 'none';
 }
-function openSubscriptionModal(){
-    document.getElementById('subscription-modal').style.display ='block';
+function openSubscriptionModal() {
+    document.getElementById('subscription-modal').style.display = 'block';
 }
 let totalAmount = 0;
-function addToCart(itemName, itemPrice){
-const cartList = document.getElementById("cart-list");
-if (!cartList) return;
-const li = document.createElement("li");
-li.innerHTML = `<span>. ${itemName}</span> <span>MWK ${Price.toLocaleString()}</span>`;
-cartList.appendChild(li);
-cartTotal += Price;
-const totalDisplay = document.getElementById("total-price");
-if (totalDisplay){
-    totalDisplay.textContent = cartTotal.toLocaleString();
+
+function addToCart(itemName, itemPrice) {
+    const cartList = document.getElementById("cart-items-list");
+    if (!cartList) return;
+    const li = document.createElement("li");
+    li.innerHTML = `
+        <span>${itemName}</span>
+        <span>MWK ${itemPrice.toLocaleString()}</span>`;
+    cartList.appendChild(li);
+    totalAmount += itemPrice;
+    const totalDisplay = document.getElementById("total-price");
+    if (totalDisplay) {
+        totalDisplay.textContent =
+            totalAmount.toLocaleString();
+    }
 }
+function clearCart(){
+    const cartList = document.getElementById("cart-items-list")
+    cartList.innerHTML ="";
+    totalAmount =0;
+    document.getElementById("total-price").textContent =
+    totalAmount.toLocaleString();
 }
